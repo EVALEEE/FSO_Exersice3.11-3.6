f429e7381cf6015f0369649334eb8453b558c559
Eva Lee <u7829183@anu.edu.au>
2025-01-26 19:10:33 +0800

    finished 3.3

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('/api/persons', (req, res) => {
 })
 
 
-// 在地址 http://localhost:3001/info 上实现一个页面, 
+// 3.2 在地址 http://localhost:3001/info 上实现一个页面, 
 // 该页面必须显示收到请求的时间和处理请求时电话簿中的条目数量。
 app.get('/info', (req, res) => {
     const info = `
@@ -41,6 +41,20 @@ app.get('/info', (req, res) => {
     <p>${new Date()}</p>
     `
     res.send(info)
+})
+
+
+// 实现显示单个电话簿条目信息的功能。获取一个 ID 为 5 的人的数据的网址应该是 http://localhost:3001/api/persons/5
+// 如果没有找到给定 ID 的条目，服务器必须以适当的状态码进行响应。
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = phonebook.find(p => p.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
 })
 
 
