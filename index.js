77c40caa7757ef2003e5a1ffba7d9e062b07fd4c
Eva Lee <u7829183@anu.edu.au>
2025-01-26 19:19:55 +0800

    finished 3.4

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/info', (req, res) => {
 })
 
 
-// 实现显示单个电话簿条目信息的功能。获取一个 ID 为 5 的人的数据的网址应该是 http://localhost:3001/api/persons/5
+// 3.3 实现显示单个电话簿条目信息的功能。获取一个 ID 为 5 的人的数据的网址应该是 http://localhost:3001/api/persons/5
 // 如果没有找到给定 ID 的条目，服务器必须以适当的状态码进行响应。
 app.get('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
@@ -56,6 +56,23 @@ app.get('/api/persons/:id', (req, res) => {
         res.status(404).end()
     }
 })
+
+
+// 3.4 实现功能，使其有可能通过向电话簿条目的唯一 URL 发出 HTTP DELETE 请求来删除单个电话簿条目。
+// 测试你的功能是否能与 Postman 或 Visual Studio Code REST 客户端一起工作。
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const personIndex = phonebook.findIndex(p => p.id === id)
+
+    if (personIndex !== -1) {
+        phonebook = phonebook.filter()(p => p.id !== id)
+        res.status(204).end()
+    } else {
+        res.status(404).end()
+    }
+})
+
+
 
 
 const PORT = 3001
