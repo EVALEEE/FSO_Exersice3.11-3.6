600507b47b254f334943298d1fa2292ec263fa02
Eva Lee <u7829183@anu.edu.au>
2025-01-26 18:58:17 +0800

    finished 3.1

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require('express')
 const app = express()
 
+// 实现一个 Node 应用，
+// 从地址 http://localhost:3001/api/persons 返回一个硬编码的电话簿条目列表。
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+const phonebook = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(phonebook)
 })
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
-})
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
